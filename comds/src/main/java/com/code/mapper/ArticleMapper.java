88700243325633aff2f40f4ac179aada040724a4
tanxuan <584701226@qq.com>
2017-03-11 18:43:10 +0800

    Add files via upload

---
+++ comds/src/main/java/com/code/mapper/ArticleMapper.java
@@ -0,0 +1,10 @@
+package com.code.mapper;
+
+import com.code.entity.Article;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by PVer on 2017/3/11.
+ */
+public interface ArticleMapper   extends Mapper<Article>{
+}
